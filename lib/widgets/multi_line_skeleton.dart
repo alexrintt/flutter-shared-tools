bf1c134942e576542975247a2ad9a1c929fac9f0
Alex Rintt <51419598+alexrintt@users.noreply.github.com>
2023-06-16 23:07:17 -0300

    Fix import issue

--- lib/widgets/multi_line_skeleton.dart
+++ lib/widgets/multi_line_skeleton.dart
@@ -1,17 +1,18 @@
+import 'package:dart_shared_tools/dart_shared_tools.dart';
 import 'package:flutter/material.dart';
 
 import '../flutter_shared_tools.dart';
 
 class MultiLineSkeleton extends StatelessWidget {
   const MultiLineSkeleton({
-    Key? key,
+    super.key,
     this.height = kToolbarHeight,
     this.count = 2,
     this.heightDashArray = const <double>[0.5],
     this.widthDashArray = const <double>[1, 0.8],
     this.borderRadius,
     this.spacing = k4dp,
-  }) : super(key: key);
+  });
 
   final double height;
   final int count;
